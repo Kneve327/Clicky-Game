d8abab6806b86d875515f7e9a916094342632322
Kneve327 <kneve327@gmail.com>
2019-01-17 20:03:01 -0500

    Updates

---
+++ precache-manifest.c9938a91906c3da99ce3b99d0774622a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "11ce45b27ac408c93a45",
+    "url": "/Clicky-Game/static/js/main.11ce45b2.chunk.js"
+  },
+  {
+    "revision": "b05f58ae460c5f47cea3",
+    "url": "/Clicky-Game/static/js/1.b05f58ae.chunk.js"
+  },
+  {
+    "revision": "11ce45b27ac408c93a45",
+    "url": "/Clicky-Game/static/css/main.f3449f2e.chunk.css"
+  },
+  {
+    "revision": "1781c8f7bc60756b6da5dbe849ff77a7",
+    "url": "/Clicky-Game/index.html"
+  }
+];
